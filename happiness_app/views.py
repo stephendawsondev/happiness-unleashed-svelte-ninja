68dae5f2c4a0b34887a13e9f27d8dbb07a38984d
Stephen Dawson <stephendawson@protonmail.com>
2024-03-21 17:55:57 +0000

    feat: add user auth to the app

--- happiness_app/views.py
+++ happiness_app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index (request):
-    return HttpResponse("Share Happiness!")
+
+
+def index(request):
+    return render(request, 'index.html')
