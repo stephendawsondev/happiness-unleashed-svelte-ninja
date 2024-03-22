e9c64f13ed54ec1ef543396827f707e32d691faa
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-03-22 09:15:12 +0000

    update gitignore and add slash to urls.py

--- acts_of_kindness/urls.py
+++ acts_of_kindness/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('acts', views.acts_list, name='acts_list'),
+    path('acts/', views.acts_list, name='acts_list'),
     path('act/<int:pk>/', views.act_detail, name='act_detail'),
     path('add/', views.add_act, name='add_act'),
     path('edit/<int:pk>/', views.edit_act, name='edit_act'),
