74aa5e3902bd38a260c5edfbf6c0564696010b3e
Claudio Cruz <claudiocruz1989@gmail.com>
2024-03-23 00:07:47 +0100

    Update navbar

--- happiness_app/views.py
+++ happiness_app/views.py
@@ -8,13 +8,13 @@ import random
 
 def index(request):
   """A view to return the homepage."""
-    acts = ActsOfKindness.objects.all().filter(
-        approved=True)[:5]
-    random_acts = random.sample(list(acts), 4) if len(acts) > 4 else acts
+  acts = ActsOfKindness.objects.all().filter(
+    approved=True)[:5]
+  random_acts = random.sample(list(acts), 4) if len(acts) > 4 else acts
     
-    context = {
-        'acts': acts,
-        'random_acts': random_acts
-    }
+  context = {
+      'acts': acts,
+      'random_acts': random_acts
+  }
 
-    return render(request, 'happiness_app/index.html', context)
+  return render(request, 'happiness_app/index.html', context)
