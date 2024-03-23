44b2503d204fd5aaf779d562a5e51b1e5f9fee17
Ann Edvinsson <aslin.edvinsson@gmail.com>
2024-03-23 13:06:33 +0000

    user can post there completed act if they want to

--- acts_of_kindness/views.py
+++ acts_of_kindness/views.py
@@ -3,13 +3,16 @@ from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import ActsOfKindness, UserProfile, UserActStatus
+from post.models import Post
 from .forms import ActsOfKindnessForm
+from post.forms import PostForm
 
-# @login_required
-
-
+@login_required
 def acts_list(request):
     """Display a list of all acts of kindness."""
+    if not request.user.is_superuser:
+       messages.error(request, 'Sorry, only admin can do that.')
+       return redirect(reverse('index'))
     acts = ActsOfKindness.objects.all()
     return render(request, 'acts_of_kindness/acts_list.html', {'acts': acts})
 
@@ -20,12 +23,12 @@ def act_detail(request, pk):
     return render(request, 'acts_of_kindness/act_detail.html', {'act': act})
 
 
-# @login_required
+@login_required
 def add_act(request):
     """Add a new act of kindness."""
-    # if not request.user.is_authenticated:
-    #   messages.error(request, 'Sorry, only logged in users can do that.')
-    #  return redirect(reverse('index'))
+    if not request.user.is_authenticated:
+        messages.error(request, 'Sorry, only logged in users can do that.')
+        return redirect(reverse('index'))
     if request.method == 'POST':
         form = ActsOfKindnessForm(request.POST, request.FILES)
         if form.is_valid():
@@ -41,12 +44,12 @@ def add_act(request):
     return render(request, 'acts_of_kindness/add_act.html', {'form': form})
 
 
-# @login_required
+@login_required
 def edit_act(request, pk):
     """Edit an existing act of kindness."""
-    # if not request.user.is_superuser:
-    #   messages.error(request, 'Sorry, only admin can do that.')
-    #  return redirect(reverse('index'))
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only admin can do that.')
+        return redirect(reverse('index'))
     act = get_object_or_404(ActsOfKindness, pk=pk)
     if request.method == 'POST':
         form = ActsOfKindnessForm(request.POST, request.FILES, instance=act)
@@ -62,32 +65,43 @@ def edit_act(request, pk):
     return render(request, 'acts_of_kindness/edit_act.html', {'form': form, 'act': act})
 
 
-# @login_required
+@login_required
 def delete_act(request, pk):
     """Delete an act of kindness."""
-    # if not request.user.is_superuser:
-    #   messages.error(request, 'Sorry, only admin can do that.')
-    #  return redirect(reverse('index'))
+    if not request.user.is_superuser:
+       messages.error(request, 'Sorry, only admin can do that.')
+       return redirect(reverse('index'))
     act = get_object_or_404(ActsOfKindness, pk=pk)
     act.delete()
     messages.success(request, 'Successfully deleted act of kindness!')
     return redirect(reverse('index'))
 
-# @login_required
-
-
-def completed_act(request, act_id):
+@login_required
+def complete_and_share_act(request, act_id):
+    """
+    Allows a user to mark an act of kindness as completed and decide if they want to share about it.
+    """
     if not request.user.is_authenticated:
         messages.error(request, 'Sorry, only logged in users can do that.')
-        return redirect(reverse('account_login'))
-    if request.method == "POST":
-        user_profile = get_object_or_404(UserProfile, user=request.user)
-        act_of_kindness = get_object_or_404(ActsOfKindness, pk=act_id)
+        return redirect(reverse('index'))
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+    act_of_kindness = get_object_or_404(ActsOfKindness, pk=act_id)
 
-        UserActStatus.objects.update_or_create(
-            profile=user_profile,
-            act_of_kindness=act_of_kindness,
-            defaults={'completed': True}
-        )
-        messages.success(request, 'Great job doing an act of kindness!')
+    UserActStatus.objects.update_or_create(
+        profile=user_profile,
+        act_of_kindness=act_of_kindness,
+        defaults={'completed': True}
+    )
+
+    # If the user chooses to share about the act, redirect to the add_post view in the post app
+    if request.method == "POST" and 'share' in request.POST:
+        return redirect('add_post', aok_pk=act_id)
+    else:
+        messages.success(request, 'Act of kindness marked as completed.')
         return redirect('profile')
+
+    # Assuming the GET request renders a page with the option to share
+    return render(request, 'acts_of_kindness/complete_and_share_act.html', {'act': act_of_kindness})
+
+
+
--- profiles/views.py
+++ profiles/views.py
@@ -7,6 +7,7 @@ import cloudinary
 from django.shortcuts import get_object_or_404
 
 from .models import UserProfile
+from acts_of_kindness.models import ActsOfKindness, UserActStatus
 
 from .forms import UserProfileForm, CustomUserEditForm
 
@@ -17,44 +18,38 @@ def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     user_form = CustomUserEditForm(instance=request.user)
     form = UserProfileForm(instance=profile)
-    completed_acts = profile.acts_of_kindness.filter(useractstatus__completed=True)
 
+    completed_acts = UserActStatus.objects.filter(profile=profile, completed=True, act_of_kindness__isnull=False)
+    
     if request.method == 'POST':
         user_form = CustomUserEditForm(request.POST, instance=request.user)
-        form = UserProfileForm(request.POST, instance=profile)
-
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
         if user_form.is_valid() and form.is_valid():
-            if request.FILES:
-                if 'profile_image' in request.FILES:
-                    image = request.FILES['profile_image']
-                    upload = cloudinary.uploader.upload(image)
-                    form.instance.profile_image = upload['url']
-
             user_form.save()
-            form.save()
+            profile_form = form.save(commit=False)
+            if 'profile_image' in request.FILES:
+                profile_form.profile_image = request.FILES['profile_image']
+            profile_form.save()
             messages.success(request, 'Profile updated successfully')
             return redirect('profile')
         else:
-            messages.error(
-                request, 'Update failed. Please ensure the form is valid.')
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
 
-    template = 'profiles/profile.html'
     context = {
         'user_form': user_form,
         'form': form,
         'profile': profile,
-        'completed_acts': completed_acts 
+        'completed_acts': completed_acts
     }
-
-    return render(request, template, context)
+    return render(request, 'profiles/profile.html', context)
 
 
 @login_required
 def account_delete(request):
-    """ Deletes the user's account and logs them out."""
+    """ Deletes the profile's account and logs them out."""
     if request.method == 'POST':
-        user = request.user
-        user.delete()
+        profile = request.profile
+        profile.delete()
         logout(request)
         messages.success(
             request, 'Your account has been successfully deleted.')
