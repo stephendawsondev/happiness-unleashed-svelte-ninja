24f0f00a73847f4934a8ec61edf1cb95614924d7
Stephen Dawson <stephendawson@protonmail.com>
2024-03-24 18:11:29 +0000

    chore: add heroku to allowed hosts

--- happiness_project/settings.py
+++ happiness_project/settings.py
@@ -35,7 +35,9 @@ DEBUG = True
 SITE_ID = 1
 
 ALLOWED_HOSTS = ['127.0.0.1', 'happiness-unleashed-b388dd1f8f63.herokuapp.com',
-                 '8000-aslinedvins-happinesapp-pu5ni4gpaax.ws-eu110.gitpod.io', '8000-zanettiprad-happinessun-dtbrvo01zpv.ws-eu110.gitpod.io',
+                 'https://happiness-unleashed-2024-4d7b660c65be.herokuapp.com/',
+                 '8000-aslinedvins-happinesapp-pu5ni4gpaax.ws-eu110.gitpod.io',
+                 '8000-zanettiprad-happinessun-dtbrvo01zpv.ws-eu110.gitpod.io',
                  '8000-fl0w97-happinesapp-hsuolreleyz.ws-eu110.gitpod.io',]
 
 CSRF_TRUSTED_ORIGINS = [
